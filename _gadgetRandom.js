0e0a7a941fd111d8462f694ac8844c66cda195c1
RetroGhosty <54449218+RetroGhosty@users.noreply.github.com>
2022-11-02 17:57:40 +0800

    v1.1

--- _gadgetRandom.js
+++ _gadgetRandom.js
@@ -1,4 +1,3 @@
-
 var typing=new Typed(".quotes", {
     strings: ["",
     "I'll just bring the sea to you",
@@ -13,3 +12,15 @@ var typing=new Typed(".quotes", {
     backSpeed: 40,
     loop: true,
 });
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount(){
+    fetch('https://api.countapi.xyz/update/redemption-dev.online/c7791c6c-e10a-4bad-9073-2964c29c148d?amount=1')
+    .then(res => res.json())
+    .then(res =>{
+        countEl.innerHTML = res.value;
+    });
+}
