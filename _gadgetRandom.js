756846965cd62cdb6e659bf70b9acbe927364307
RetroGhosty <54449218+RetroGhosty@users.noreply.github.com>
2022-11-03 13:16:25 +0800

    v1.2

--- _gadgetRandom.js
+++ _gadgetRandom.js
@@ -1,7 +1,6 @@
 var typing=new Typed(".quotes", {
     strings: ["",
     "I'll just bring the sea to you",
-    "Faded than a hoe",
     "Don't believe them things they tell you 'cause it ain't true",
     "You're losing your gift and it's plain to see",
     "Look at the sky",
