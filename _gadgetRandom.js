8438a711faca532091cf5511e28de06ea26afd6b
talipapa <54449218+RetroGhosty@users.noreply.github.com>
2022-11-11 10:51:32 +0800

    Update _gadgetRandom.js

--- _gadgetRandom.js
+++ _gadgetRandom.js
@@ -1,15 +1,10 @@
 var typing=new Typed(".quotes", {
     strings: ["",
-    "I'll just bring the sea to you",
-    "Don't believe them things they tell you 'cause it ain't true",
-    "You're losing your gift and it's plain to see",
     "Look at the sky",
-    "I'm not better than you",
-    ""
     ],
     typeSpeed: 50,
     backSpeed: 40,
-    loop: true,
+    loop: false,
 });
 
 const countEl = document.getElementById('count');
